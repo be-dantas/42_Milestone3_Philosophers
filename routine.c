00fa9c03e9e5c06195ce63cd8d6f982b45db6ea0
Beatriz Dantas <226878089+be-dantas@users.noreply.github.com>
2025-12-04 17:43:46 -0300

    update

--- routine.c
+++ routine.c
@@ -6,7 +6,7 @@
 /*   By: bedantas <bedantas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 14:58:43 by bedantas          #+#    #+#             */
-/*   Updated: 2025/12/04 17:10:02 by bedantas         ###   ########.fr       */
+/*   Updated: 2025/12/04 17:43:33 by bedantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 static void print_action(t_philo *p, char *str)
 {
-	long    t_now;
-	long    time;
+	long	t_now;
+	long	time;
 	
 	pthread_mutex_lock(&p->data->print_lock);
 	if (p->data->flag_died != 1)
@@ -38,34 +38,44 @@ void	*philo_routine(void *arg)
 	while (p->data->flag_died != 1)
 	{
 		pthread_mutex_lock(p->left_fork);
-		print_action(&p, "has taken a fork");
+		print_action(p, "has taken a fork");
+		
 		if (p->data->inp.n_philo == 1)
 		{
 			usleep(p->data->inp.time_to_die * 1000);
-			print_action(&p, "died");
-			p->data->flag_died == 1;
 			break ;
 		}
+		
 		pthread_mutex_lock(p->right_fork);
-		print_action(&p, "has taken a fork");
-		print_action(&p, "is eating");
+		print_action(p, "has taken a fork");
+		
+		print_action(p, "is eating");
 		usleep(p->data->inp.time_to_eat * 1000);
+		p->time_finish_eat = time_now(&p->data);
+		
 		pthread_mutex_unlock(p->left_fork);
 		pthread_mutex_unlock(p->right_fork);
-		print_action(&p, "is sleeping");
+		
+		print_action(p, "is sleeping");
 		usleep(p->data->inp.time_to_sleep * 1000);
-		print_action(&p, "is thinking");
-		p->time_finish = time_now(&p->data);
-		if (p->time_finish > p->data->inp.time_to_die)
-		{
-			print_action(&p, "died");
-			p->data->flag_died == 1;
-			break ;
-		}
+		
+		print_action(p, "is thinking");
 	}
 }
 
-void	monitor(t_data *data)
-{
+// void	monitor(t_data *data)
+// {
+// 	long	t_now;
+
+// 	while (data->flag_died != 1)
+// 	{
+// 		t_now = time(&data);
+// 		if (t_now - data->philos[i].time_finish_eat > data->inp.time_to_die)
+// 		{
+// 			print_action(&data->philos[i], "died");
+// 			data->flag_died = 1;
+// 		}
+// 	}
 	
-}
+
+// }
